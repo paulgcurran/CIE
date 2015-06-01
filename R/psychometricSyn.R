15418b50b81adfadb7108cb2cfeb7f57b2586c0d
paulgcurran <paulgcurran@gmail.com>
2015-06-01 11:02:50 -0400

    Added more comments to file

--- R/psychometricSyn.R
+++ R/psychometricSyn.R
@@ -12,7 +12,9 @@
 #' psychometricSyn(values)
 
 
-######### function 4 - psychometric synonyms
+##function 4 - psychometric synonyms
+
+##creating file for combination of metrics
 
 psychometricSyn<-function(Data1){
   x<-exists("Data2")
@@ -21,17 +23,20 @@ psychometricSyn<-function(Data1){
   }
   rm(x)
   
-  # set number of items from the dimensions of the input file
+  ##set number of items from the dimensions of the input file
   dim<-dim(Data1)
   nItems<-dim[2]
   #nItems<-50
   
-  # need to pick how many pairs of synonyms used (default to 5)
+  ##need to pick how many pairs of antonyms used (default to top 5)
+  ####need to modify this to allow for user inputs
   nPairs<-5
   
-  # need to set minimum value for selected correlations (default to -.5)
+  ##need to set minimum value for selected correlations (default to .5)
+  ###need to modify this to allow for user inputs
   minCor<-.5
   
+  ##finding correlations
   cors<-cor(Data1,use="pairwise.complete.obs")
   #hist(cors)
   cors[cors==1]<-NA
@@ -41,6 +46,7 @@ psychometricSyn<-function(Data1){
   print(corsOrder[1:10])
   topCors<-cors[topPairs]
   print(topCors)
+  ##displays an error message if any correlations below min
   if ((topCors[10])<minCor) {print("WARNING:the correlation of at least one pair of items is below the minimum threshold.")
   }  else {print("All correlations are above the minimum threshold.")
   } 
@@ -49,15 +55,15 @@ psychometricSyn<-function(Data1){
   rowPlaceVect<-NULL
   columnPlaceVect<-NULL
   
-  #building constraints for while loops
+  ##building constraints for while loops
   dim<-dim(Data1)
   persons<-dim[1]
   x<-0
   
-  #creating empty variable to be filled by while loop
+  ##creating empty variable to be filled by while loop
   Data2$psychSynonyms<-NULL
   
-  # creating vectors of pairs of items
+  ##creating vectors of top pairs of items
   while (x<10) {
     x<-x+1;
     rowPlace<-ceiling(topPairs[x]/nItems);
@@ -71,6 +77,9 @@ psychometricSyn<-function(Data1){
   }
   
   j<-0
+  
+  ##iterating over people
+  
   while (j<persons) {
     j<-j+1
     first<-c(Data1[j,rowPlaceVect]);
@@ -82,6 +91,7 @@ psychometricSyn<-function(Data1){
     rm(first,second,vFirst,vSecond);
   }
   
+  ##older code for displaying values
   # display data file with appended data
   #Data2
   # generate basic stats of person total correlations
@@ -90,8 +100,10 @@ psychometricSyn<-function(Data1){
   hist(Data2$psychSynonyms, breaks=50, col="blue")
   
   psychSynonyms<-Data2$psychSynonyms
+  ##returns value for user to store
   return(psychSynonyms)
   
+  ##cleaning remaining variables
   rm(cors,columnPlaceVect,corsOrder,dim,j,minCor,nItems,nPairs,rowPlaceVect,topCors,topPairs,x,persons)
   
 
