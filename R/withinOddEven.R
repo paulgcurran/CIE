5b3f007ea58607f50c708a3d8106b2a81038d7c2
paulgcurran <paulgcurran@gmail.com>
2015-06-02 09:38:41 -0400

    Added more comments to file

--- R/withinOddEven.R
+++ R/withinOddEven.R
@@ -12,7 +12,9 @@
 #' withinOddEven(values)
 
 
-########### function 2 - odd-even within person correlations
+##function 2 - odd-even within person correlations
+
+##creating file for later use of higher level commands
 
 withinOddEven<-function(Data1){
   x<-exists("Data2")
@@ -20,27 +22,32 @@ withinOddEven<-function(Data1){
     Data2<-Data1
   }
   rm(x)
-  # set number of items from the dimensions of the input file
+  ##set number of items from the dimensions of the input file
   dim<-dim(Data1)
   nItems<-dim[2]
   #nItems<-50
   
-  # if odd number of items, disregard the final one
+  ##if odd number of items, disregard the final one
+  ##not sure if this should be a user choice, or just default
+  ##what is currently done in practice?
   if (nItems%%2==1) {
     nItems<-nItems-1
   }
-  #building sequences of numbers
+  
+  ##building sequences of numbers
   oddItems<-seq(1,nItems,2)
   evenItems<-seq(2,nItems,2)
-  #building constraints for while loop
+  ##building constraints for while loop
   #dim<-dim(Data1)
   persons<-dim[1]
   x<-0
-  #creating empty variable to be filled by while loop
+  ##creating empty variable to be filled by while loop
   Data2$coefOddEven<-NULL
   
-  #iteratively computing calculation within each person, populating new variable
+  ##iteratively computing calculation within each person, populating new variable
   
+  ##this is rather simple, just correlation for each person
+  ##no need to iterate over items like other metrics
   while (x<persons) {
     x<-(x+1);
     odd<-c(Data1[x,oddItems]);
@@ -52,7 +59,7 @@ withinOddEven<-function(Data1){
     rm(odd,even,vOdd,vEven)
   }
   
-  
+  ##older code for displaying variables
   # display data file with appended data
   #Data2
   # generate basic stats of person total correlations
@@ -61,8 +68,10 @@ withinOddEven<-function(Data1){
   hist(Data2$coefOddEven, breaks=50, col="blue")
   
   coefOddEven<-Data2$coefOddEven
+  ##returns values for user to store
   return(coefOddEven)
   
+  ##cleaning remaining variables
   rm(dim,evenItems,nItems,oddItems,x,persons)
   
   
