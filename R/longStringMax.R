7fda5006aa3ebced0f3f4ffc93bd7f51e938304e
paulgcurran <paulgcurran@gmail.com>
2018-08-09 16:00:16 -0400

    -Updated to allow for user to call either the value of the long string coefficient, the response option with the longest string, or both.
    - Fixed an error impacting fully invariant response strings.

--- R/longStringMax.R
+++ R/longStringMax.R
@@ -16,90 +16,96 @@
 
 ##creating copy dataset for use in running all functions at once
 
-longStringMax<-function(Data1){
+longStringMax<-function(Data1,scoreReport="counts"){
   x<-exists("Data2")
   if(x==FALSE){
     Data2<-Data1
   }
   rm(x)
-
-##set number of items from the dimensions of the input file
-dim<-dim(Data1)
-nItems<-dim[2]
-persons<-dim[1]
-##creating variables to be populated
-longResponse<-NULL
-Data2$longLength<-NULL
-Data2$longResponse<-NULL
-nItemsR<-nItems-1
-x<-0
-
-##creating file to work with to account for NA
-Data3<-Data1
-Data3[is.na(Data3)==TRUE]<-(-99)
-
-##outer loop to iterate over people
-##inner loop to generate values
-
-for (x in 1:persons){
   
-##minimum value this can ever be is 1  
-  longLength<-1
-  length<-1
+  ##set number of items from the dimensions of the input file
+  dim<-dim(Data1)
+  nItems<-dim[2]
+  persons<-dim[1]
+  ##creating variables to be populated
+  longResponse<-NULL
+  Data2$longLength<-NULL
+  Data2$longResponse<-NULL
+  nItemsR<-nItems-1
+  x<-0
   
-  for (k in 1:nItemsR){
+  ##creating file to work with to account for NA
+  Data3<-Data1
+  Data3[is.na(Data3)==TRUE]<-(-99)
+  
+  ##outer loop to iterate over people
+  ##inner loop to generate values
+  
+  for (x in 1:persons){
     
-##checking if adjacent values are the same, iterating    
-    if (Data3[x,k]==Data3[x,k+1]){
-      response<-Data3[x,k]
-      length<-length+1
-    } 
-##if values are differnet need to restart counting    
-    else {
-      if (length>longLength) {
-        longLength<-length
-        longResponse<-response
-      } 
-##if current cycle longer than prior, writes to current value      
-      if (k==nItemsR){
+    ##minimum value this can ever be is 1  
+    longLength<-1
+    length<-1
+    
+    for (k in 1:nItemsR){
+      
+      ##checking if adjacent values are the same, iterating    
+      if (Data3[x,k]==Data3[x,k+1]){
+        response<-Data3[x,k]
+        length<-length+1
+        #storing value if current longest
         if (length>longLength) {
           longLength<-length
           longResponse<-response
         } 
+      } 
+      #if values are different, resetting initial counts
+      else {
+        length<-1
+        response<-0
       }
-      length<-1
-    }
+      
+    } 
+    
+    
+    ##some cleaning and storing from earlier versions
+    Data2$longLength[x]<-longLength
+    Data2$longResponse[x]<-longResponse
+    #Data2$longLength[x]<<-longLength
+    #Data2$longResponse[x]<<-longResponse
+    #rm(longLength,longResponse)
+    
   }
   
-##some cleaning and storing from earlier versions
-  Data2$longLength[x]<-longLength
-  Data2$longResponse[x]<-longResponse
-  #Data2$longLength[x]<<-longLength
-  #Data2$longResponse[x]<<-longResponse
-  #rm(longLength,longResponse)
+  # display data file with appended data
+  #Data2
+  # generate basic stats of person total correlations
+  summary(Data2$longLength)
+  summary(Data2$longResponse)
+  ##generate histogram of calculated values
+  hist(Data2$longLength, breaks=10, col="blue")
+  hist(Data2$longResponse, breaks=10, col="blue")
   
-}
-
-# display data file with appended data
-#Data2
-# generate basic stats of person total correlations
-summary(Data2$longLength)
-summary(Data2$longResponse)
-##generate histogram of calculated values
-hist(Data2$longLength, breaks=10, col="blue")
-hist(Data2$longResponse, breaks=10, col="blue")
-
-##generate scores to return, user can store as values
-rm(longLength,longResponse)
-longLength<-NULL
-longResponse<-NULL
-longLength<-Data2$longLength
-longResponse<-Data2$longResponse
-vals<-list(longLength,longResponse)
-return(vals)
-
-##cleaning remaining variables
-rm(dim,k,length,nItems,nItemsR,persons,response,x)
-
-
+  ##generate scores to return, user can store as values
+  rm(longLength,longResponse)
+  longLength<-NULL
+  longResponse<-NULL
+  longLength<-Data2$longLength
+  longResponse<-Data2$longResponse
+  vals<-list(longLength,longResponse)
+  if(scoreReport=="counts"){
+    return(longLength)
+  }
+  if(scoreReport=="response"){
+    return(longResponse)
+  }
+  if(scoreReport=="both"){
+    return(vals)  
+  }    
+  
+  
+  ##cleaning remaining variables
+  rm(dim,k,length,nItems,nItemsR,persons,response,x)
+  
+  
 }
