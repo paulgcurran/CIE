08aef9abdfc214f67776640baa605c6007bdbed2
paulgcurran <paulgcurran@gmail.com>
2018-09-28 16:55:44 -0400

    - General updates: - Updated to allow for aggregation over scales - Bug fixes

--- R/interItemSD.R
+++ R/interItemSD.R
@@ -16,7 +16,7 @@
 
 ##creating file for later use of higher level commands
 
-interItemSD<-function(Data1){
+interItemSD<-function(Data1,scales=1,scaleAgg="mean"){
   x<-exists("Data2")
   if(x==FALSE){
     Data2<-Data1
@@ -28,41 +28,88 @@ interItemSD<-function(Data1){
   ##set number of people from dimensions of input file
   nPeople<-dim[1]
   
-##creating variables to populate
-interItemSD<-NULL
-personMean<-NULL
-x<-NULL
-num<-NULL
-den<-NULL
-
-##iterates over individuals
-
-for (i in 1:nPeople){
-  x<-t(Data1[i,])
-  personMean<-mean(x)
-  den<-(nItems-1)
-  num<-0
-
-  ##iterates over items
-  for (j in 1:nItems){
-    val<-(Data1[i,j]-personMean)
-    val2<-val*val
-    num<-num+val2
-    val<-NULL
-    val2<-NULL
+  ##creating variables to populate
+  interItemSD<-NULL
+  personMean<-NULL
+  x<-NULL
+  num<-NULL
+  den<-NULL
+  
+  ##iterates over individuals
+  if(scales==1){
+    for (i in 1:nPeople){
+      x<-t(Data1[i,])
+      personMean<-mean(x)
+      den<-(nItems-1)
+      num<-0
+      
+      ##iterates over items
+      for (j in 1:nItems){
+        val<-(Data1[i,j]-personMean)
+        val2<-val*val
+        num<-num+val2
+        val<-NULL
+        val2<-NULL
+      }
+      
+      vari<-(num/den)
+      interItemSD[i]<-(sqrt(vari))
+      
+      x<-NULL
+      personMean<-NULL
+      den<-NULL
+      num<-NULL
+      vari<-NULL
+    }
   }
   
-  vari<-(num/den)
-  interItemSD[i]<-(sqrt(vari))
   
-  x<-NULL
-  personMean<-NULL
-  den<-NULL
-  num<-NULL
-  vari<-NULL
-}
-
-return(interItemSD)
-rm(den,num,personMean,val,val2,vari,x)
-
+  
+  if(scales>1){
+    interItemSD2<-matrix(ncol=scales,nrow=nPeople)
+    
+    f<-(scales-1)
+    g<-(nItems/scales)
+    
+    #looping over scales
+    for(w in 0:f){
+      a<-((w*g)+1)
+      b<-((w*g)+g)
+      
+      for (i in 1:nPeople){
+        x<-t(Data1[i,a:b])
+        personMean<-mean(x)
+        den<-(g-1)
+        num<-0
+        
+        ##iterates over items
+        for (j in 1:g){
+          val<-(Data1[i,j]-personMean)
+          val2<-val*val
+          num<-num+val2
+          val<-NULL
+          val2<-NULL
+        }
+        
+        vari<-(num/den)
+        interItemSD2[i,w+1]<-(sqrt(vari))
+        
+        x<-NULL
+        personMean<-NULL
+        den<-NULL
+        num<-NULL
+        vari<-NULL
+      }
+    }
+    
+    #collapses the scale scores
+    for(i in 1:nPeople){
+      interItemSD[i]<-mean(interItemSD2[i,1:scales])
+    }
+    
+    return(interItemSD)
+    rm(den,num,personMean,val,val2,vari,x)
+    
+  }
+  
 }
